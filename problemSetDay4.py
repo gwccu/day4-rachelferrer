6ef6b51e52f6dd4e0bc6f5cbc97404f4d2a29c0a
rachelferrer <rferrer@beaconschool.org>
2019-02-23 17:29:04 +0000

    ProblemSetDay3!!!

--- problemSetDay4.py
+++ problemSetDay4.py
@@ -1,1 +1,40 @@
 # file name: problemSetDay4.py
+print("How studious are you?")
+print("Question 1")
+answer = input("How many hours a day do you spend doing homework? "+ "Less than an hour, one hour, or more than an hour?")
+if answer == "less than an hour":
+    print("Hm. You're pretty fast... maybe check over it?")
+elif answer == "one hour":
+    print("Cool. That's about average.")
+elif answer == "more than an hour":
+    print("WOW.")
+else:
+    print("Look over the choices again!")
+print("Question 2")
+answer2 = input("When your teacher mentions that there will be a test next week... when do you start studying? "+ "The moment you get home, the beginning of next weeK, or the night before the test?")
+if answer2 == "The moment I get home":
+    print("Look at you smarty pants!")
+elif answer2 == "The beginning of next week":
+    print("Okay, you're quite the study bug.")
+elif answer2 == "The night before the test":
+    print("C'mon now!")
+else:
+    print("Look over the choices again!")
+print("Question 3")
+answer3 = input("Do you look over your class notes after you've taken them? "+ "No, never!, Only when I don't really understand the material, EVERY NIGHT!" )
+if answer3 == "No, never!":
+    print("Why not? It could be helpful!")
+elif answer3 == "Only when I don't really understand the material.":
+    print("Really good habit, maybe go for tutoring!")
+elif answer3 == "EVERY NIGHT!":
+    print("Okay, you go girl! Stay studious.")
+print("Question 4")
+answer4 = input("Do you complete your homeowork and assignments on time? " + "Not really, Most of the time, or all the time!")
+if answer4 == "Not really!"
+    print("You're not very studious. Let's work on improving!")
+elif answer4 == "Most of the time"
+    print("You're pretty studious")
+elif answer4 == "All the time!"
+    print("You're the ,pst studious of them all!")
+else:
+    print("Maybe look over our choices again!")
