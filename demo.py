e13c72918d8e1230f79e7ae82bfdd42aeb2c3cbb
rachelferrer <rferrer@beaconschool.org>
2019-02-23 16:35:20 +0000

    hi!

---
+++ demo.py
@@ -0,0 +1,9 @@
+weather = input("What is the weather today?")
+if weather == "sunny":
+    print("Wear shorts today!")
+elif weather == "rainy":
+    print("Bring an umbrella.")
+elif weather == "windy":
+    print("Wear a jacket.")
+else:
+    print("Does not recognize weather.")
